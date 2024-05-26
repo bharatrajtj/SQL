aa88365232ed951743ebf017b36d4ea6af687bde
bharatrajtj <65009556+bharatrajtj@users.noreply.github.com>
2024-05-26 10:52:30 -0400

    Create 2.1378Join.sql

---
+++ 2.1378Join.sql
@@ -0,0 +1,50 @@
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Employees table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI table:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
+
+
+select unique_id, name  ######(select the column need for final output_)
+FROM Employees     #### This is the main table
+LEFT JOIN EmployeeUNI  #### Left Join is applied to main table = Employees
+ON Employees.id = EmployeeUNI.id  ### Common link between two table)
